ee686357d1cf60ec5a9ab51268d1c9630490ba06
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2022-08-07 17:23:14 +0300

    wrapper

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+    <header>
+      
+      
+      <div className ="headerLeft">
+        <img src = "/img/logo.png"/>
+        <div className = "headerInfo">
+        <h3>
+          React Sneakers
+        </h3>
+        <p>Maгазин лучших кроссовок</p>
+      </div>
+      </div>
+      <ul className = "headerRight">
+        <li>
+        <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 1H4.27273L6.46545 11.9555C6.54027 12.3321 6.7452 12.6705 7.04436 12.9113C7.34351 13.1522 7.71784 13.2801 8.10182 13.2727H16.0545C16.4385 13.2801 16.8129 13.1522 17.112 12.9113C17.4112 12.6705 17.6161 12.3321 17.6909 11.9555L19 5.09091H5.09091" stroke="#9B9B9B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+ 
+          <span>1505 руб.</span>
+          </li>
+      <li>
+      <svg /> 
+         
+        </li>
+      </ul>
+    </header>
+    <div className= "content">
+      <h1>Все кроссовки</h1>
+      ...
+       </div>
     </div>
   );
 }
