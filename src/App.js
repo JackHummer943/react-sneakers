a0e3652d7710460388b04062a5497f389b0f6d27
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2022-08-15 10:48:05 +0300

    sneaker5

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import React from 'react';
 function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([ ]);
+  const [searchValue, setSearchValue] = React.useState(['']);
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
@@ -23,6 +24,10 @@ function App() {
 const onAddToCart = (obj) => {
  setCartItems((prev) => [... prev, obj]);
 };
+const onChangeSearchInput = (event) => {
+  console.log(event.target.value);
+  setSearchValue(event.target.value);
+}
 
 
   return (
@@ -31,16 +36,18 @@ const onAddToCart = (obj) => {
     <Header onClickCart = {() => setCartOpened(true)}/>
     <div className= "content  p-40">
       <div className="d-flex align-center justify-between mb-40">
-        <h1>Все кроссовки</h1>
+        <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : `Все кроссовки`}</h1>
         <div className="search-block d-flex">
         <img src="/img/search.svg" alt="Search" />
-        <input placeholder="Поиск..."></input>
+        <img  className="cu-p" src ="img/btn-remove.svg " alt="Remove"/>
+        <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..."></input>
         </div>
       </div>
 
       <div className="d-flex flex-wrap">
-        {items.map((item) =>( 
+        {items.map((item, index) =>( 
           <Card 
+          key = {index}
           title ={item.title}
           price={item.price} 
           imageUrl={item.imageUrl}
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ function Header (props) {
     return (
 <header className ="d-flex justify-between align-center p-40">
       <div className ="d-flex aliign-center">
-        <img width ={40} height = {40} src = "/img/logo.png"/>
+        <img width ={40} height = {40} src = "/img/logo.png" alt ="logotype "/>
         <div>
         <h3 className="text-uppercase">
           React Sneakers
@@ -13,11 +13,11 @@ function Header (props) {
       </div>
       <ul className = "d-flex">
         <li onClick={props.onClickCart} className = "mr-30 cu-p">
-        <img width={18} height={18} src = "/img/vector.svg"/>
+        <img width={18} height={18} src = "/img/vector.svg" alt ="Cart"/>
           <span>1505 руб.</span>
           </li>
       <li >
-      <img width ={18} height = {18} src="/img/union.svg"/> 
+      <img width ={18} height = {18} src="/img/union.svg" alt = "User"/> 
          
         </li>
       </ul>
