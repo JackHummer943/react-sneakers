d5d6928c9cfb0e6baf11dc21fe594737f93c9446
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2022-08-16 10:43:55 +0300

    sneaker5.2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Card from './components/Card';
+import axios from 'axios';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 import React from 'react';
@@ -12,18 +13,28 @@ function App() {
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
-    fetch('https://62f72b81ab9f1f8e89f9780a.mockapi.io/items')
-  .then((res) => {
-    return res.json();
-  })
-  .then((json) => {
-    setItems(json);
+
+
+  axios.get('https://62f72b81ab9f1f8e89f9780a.mockapi.io/items').then((res) => {
+    setItems(res.data);
   });
+  axios.get('https://62f72b81ab9f1f8e89f9780a.mockapi.io/cart').then((res) => {
+    setCartItems(res.data);
+  });
+
+
   }, []);
 
 const onAddToCart = (obj) => {
+  axios.post('https://62f72b81ab9f1f8e89f9780a.mockapi.io/cart', obj);
  setCartItems((prev) => [... prev, obj]);
 };
+
+const onRemoveItem = (id) => {
+  axios.delete('https://62f72b81ab9f1f8e89f9780a.mockapi.io/cart/${id}');
+//  setCartItems((prev) => [... prev, obj]);
+
+}
 
 const onChangeSearchInput = (event) => {
   setSearchValue(event.target.value);
@@ -32,7 +43,7 @@ const onChangeSearchInput = (event) => {
 
   return (
     <div className="wrapper clear">
-    {cartOpened && <Drawer items = {cartItems} onClose= {() => setCartOpened(false)}/>}
+    {cartOpened && <Drawer items = {cartItems} onClose= {() => setCartOpened(false)} onRemove={onRemoveItem}/>}
     <Header onClickCart = {() => setCartOpened(true)}/>
     <div className= "content  p-40">
       <div className="d-flex align-center justify-between mb-40">
@@ -53,7 +64,7 @@ const onChangeSearchInput = (event) => {
 
       <div className="d-flex flex-wrap">
         {items
-        .filter((item) => item.title.includes(searchValue))
+        .filter((item) => item.title.toLowerCase().includes(searchValue))
         .map((item, index) => ( 
           <Card 
           key = {index}
