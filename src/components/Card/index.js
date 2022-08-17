f0ffa9d6ad185070e15ea1559ed5236ff23316c6
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2022-08-17 10:44:56 +0300

    sneaker5.4

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,18 +3,21 @@ import styles from './Card.module.scss';
 
 function Card({  title, imageUrl, price, onFavorite, onPlus }) {
   const [isAdded, setIsAdded] = React.useState(false);
+  const [isFavorite, setIsFavorite] = React.useState(false);
 
   const onClickPlus = () => {
     onPlus({title, imageUrl, price});
     setIsAdded(!isAdded);
   };
-
+  const onClickFavorite = () => {
+    
+  }
 
 
     return (
         <div className={styles.card}>
           <div className={styles.favorite} onClick={onFavorite}>
-        <img src ="/img/heart-unliked.svg" alt="Unliked"/>
+        <img src ={isFavorite ? '/img/liked.svg' : '/img/unliked.svg'} alt="Unliked"/>
         </div>
         <img width = {133} height = {112} src={imageUrl} alt ="Sneakers "/>
         <h5>{title}</h5>
